1fb098a6d775229f84289eb243d3961669ca5132
yzq <1466490372@qq.com>
2017-09-19 17:37:54 +0800

    struts2

---
+++ src/main/java/com/zhiyou/struts2/action/HelloAction.java
@@ -0,0 +1,15 @@
+package com.zhiyou.struts2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class HelloAction extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		
+	
+		
+		return "test";
+	}
+}
---
+++ src/main/java/com/zhiyou/struts2/action/LoginAction.java
@@ -0,0 +1,50 @@
+package com.zhiyou.struts2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class LoginAction extends ActionSupport {
+	
+	private String userid;
+	private String pwd;
+	
+	/** 参数校验 */
+	public void validate(){
+		// 验证非空
+		if (userid==null || "".equals(userid.trim())) {
+			// 添加错误信息
+			super.addFieldError("userid", "不能为空");
+			super.addFieldError("userid", "不能为空2");
+		}
+	}
+	
+	@Override
+	public String execute(){
+		if ("智游".equals(userid) && "123".equals(pwd)) {
+			return SUCCESS;
+		}else if("zhiyou".equals(userid) && "123".equals(pwd)){
+			super.addActionError("用户名或密码错误");
+			return INPUT;
+		}
+		// 返回跳转路径名称
+		return "forward";
+		
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
